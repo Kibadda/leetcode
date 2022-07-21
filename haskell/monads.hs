397b18f9caabc82a38fadeaebacb574206105321
Michael Strobel <mstrobel97@gmail.com>
2022-07-21 15:58:04 +0200

    monads !!!!!1111elf!!!

--- haskell/monads.hs
+++ haskell/monads.hs
@@ -1,17 +1,39 @@
-data WithLog a = WithLog {value :: a, valueLog :: [String]} deriving (Show)
+data WithLog a = WithLog {getValue :: a, getLog :: [String]} deriving (Show)
 
-wrap :: a -> WithLog a
-wrap x = WithLog {value = x, valueLog = []}
+instance Functor WithLog where
+  fmap f (WithLog x log) = WithLog (f x) log
 
-run :: WithLog a -> (a -> WithLog b) -> WithLog b
-run WithLog {value = v, valueLog = l} fun =
-  WithLog {value = newValue, valueLog = newLog ++ l}
-  where
-    newWithLog = fun v
-    newValue = value newWithLog
-    newLog = valueLog newWithLog
+instance Applicative WithLog where
+  pure f = WithLog f []
+  (WithLog f log) <*> something = fmap f something
+
+instance Monad WithLog where
+  return x = WithLog x []
+  WithLog x log >>= f = WithLog x' (log ++ log')
+    where
+      WithLog x' log' = f x
 
 add :: Int -> Int -> WithLog Int
-add x y = WithLog {value = newNumber, valueLog = ["added " ++ show x ++ " to " ++ show y ++ " to get " ++ show newNumber]}
+add x y = WithLog value log
   where
-    newNumber = x + y
+    value = x + y
+    log = ["added " ++ show x ++ " to " ++ show y ++ " to get " ++ show value]
+
+mul :: Int -> Int -> WithLog Int
+mul x y = WithLog value log
+  where
+    value = x * y
+    log = ["multiplied " ++ show x ++ " with " ++ show y ++ " to get " ++ show value]
+
+sub :: Int -> Int -> WithLog Int
+sub x y = WithLog value log
+  where
+    value = y - x
+    log = ["subtracted " ++ show x ++ " from " ++ show y ++ " to get " ++ show value]
+
+calculate :: WithLog Int
+calculate = do
+  start <- return 5 :: WithLog Int
+  step1 <- add 10 start
+  step2 <- mul 3 step1
+  sub 9 step2
